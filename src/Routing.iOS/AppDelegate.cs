bccd9d8d234ceb55416252f66549680854201d8d
Rodney Littles II <rodney.littles@gmail.com>
2018-05-25 00:28:39 -0500

    Fixed ordering of registration vs app creation

--- src/Routing.iOS/AppDelegate.cs
+++ src/Routing.iOS/AppDelegate.cs
@@ -9,22 +9,22 @@ using UIKit;
 
 namespace Routing.iOS
 {
-    [Register("AppDelegate")]
-    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
-    {
-        public override bool FinishedLaunching(UIApplication application, NSDictionary options)
-        {
-            //RxApp.DefaultExceptionHandler = null;
+	[Register("AppDelegate")]
+	public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+	{
+		public override bool FinishedLaunching(UIApplication application, NSDictionary options)
+		{
+			//RxApp.DefaultExceptionHandler = null;
 
-            global::Xamarin.Forms.Forms.Init();
-            var compositionRoot = new iOSCompositionRoot();
-            var app = compositionRoot.ResolveApp();
-            var registrar = new iOSDependencyRegistrar();
-            registrar.Register(Locator.CurrentMutable, compositionRoot);
-            app.Initialize();
+			global::Xamarin.Forms.Forms.Init();
+			var compositionRoot = new iOSCompositionRoot();
+			var registrar = new iOSDependencyRegistrar();
+			registrar.Register(Locator.CurrentMutable, compositionRoot);
+			var app = compositionRoot.ResolveApp();
+			app.Initialize();
 
-            LoadApplication(app);
-            return base.FinishedLaunching(application, options);
-        }
-    }
+			LoadApplication(app);
+			return base.FinishedLaunching(application, options);
+		}
+	}
 }
